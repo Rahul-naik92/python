dc12aa67e31a64deb781fdf689b8b4753e222253
rahulnaik92 <teamrahul.naik@gmail.com>
2025-10-16 10:32:35 +0530

    append and print list code

--- linked_list/linked_list_constructor.py
+++ linked_list/linked_list_constructor.py
@@ -10,20 +10,35 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
-        #def append(value):
-        #def prepend(value):
-        #def insert(self,value, position):
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next   
+
+    def append(self,value):
+            new_node = Node(value)
+            if self.head is None:
+                self.head = new_node
+                self.tail = new_node
+            else:
+                self.tail.next = new_node
+                self.tail = new_node
+            self.length += 1
+            return True
+    #def prepend(value):
+    #def insert(self,value, position):
+
+  
+            
 
 my_linked_list = LinkedList(1)
-print(my_linked_list)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.print_list()
 
 
 
 
 
-def print_list(node):
-    temp = node
-    while temp is not None:
-        print(temp.value)
-        temp = temp.next    
-            
