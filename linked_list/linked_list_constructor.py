624173fea508f7d2107f08677e849e69529f1114
rahulnaik92 <teamrahul.naik@gmail.com>
2025-10-17 23:45:44 +0530

    linked list append and push

--- linked_list/linked_list_constructor.py
+++ linked_list/linked_list_constructor.py
@@ -27,7 +27,48 @@ class LinkedList:
                 self.tail = new_node
             self.length += 1
             return True
-    #def prepend(value):
+    def pop(self):
+            if self.length == 0:
+                return None
+            temp = self.head
+            pre = self.head
+            while(temp.next):
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return temp.value
+    def prepend(self,value):
+            new_node = Node(value)
+            if self.length == 0:
+                self.head = new_node
+                self.tail = new_node
+            else:
+                new_node.next = self.head
+                self.head = new_node
+            self.length += 1
+            return True
+    def pop_first(self):
+            if self.length == 0:
+                return None
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.tail = None
+            return temp.value  
+    def get(self, index):
+            if index < 0 or index >= self.length:
+                return None
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            return temp.value
     #def insert(self,value, position):
 
   
@@ -36,6 +77,16 @@ class LinkedList:
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
 my_linked_list.append(3)
+my_linked_list.print_list()
+print("================================")
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print("================================")
+my_linked_list.prepend(0)
+print("================================")
+print(my_linked_list.pop_first())
+print("==============pop_first==================")
+print(my_linked_list.get(0))# my_linked_list.insert(4,1)
 my_linked_list.print_list()
 
 
