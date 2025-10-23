f3f9a37bb0f47f1280186c0cc31261a77ee09a39
rahulnaik92 <teamrahul.naik@gmail.com>
2025-10-23 09:33:10 +0530

    Add variables and data structures examples to helloworld.py

--- 01-Intro/helloworld.py
+++ 01-Intro/helloworld.py
@@ -1,1 +1,30 @@
 print("Hello World!")
+
+#String Variable
+
+skill="DevOps"
+print  (skill)
+
+#Integer Variable
+age=25
+print (age)
+
+#List Variable
+fruits=["Mango","Apple","Banana"]
+print (fruits)
+print (fruits[0])
+fruits.append("Orange")
+print (fruits)
+#fruits.add(0,"Grapes")
+
+#Tuple Variable
+dimensions=(200,5,0)
+print (dimensions)
+
+
+#Dictionary Variable
+person={"name":"John","age":30,"city":"New York"}
+print (person)
+print (person["name"])
+print (person["age"])
+print (person["city"])
